fb8d4cad4a21793310d314c3e6e46d2363f440db
Edoardo Zanoni <edoardo.zanoni@gambabruno.it>
2017-11-03 14:49:15 +0100

    ~ Modificato nome icona

--- installer/windows/make.bat
+++ installer/windows/make.bat
@@ -6,6 +6,6 @@
 	CD files
 	SET prg_name=intellij-idea
 	SET files=lib raiser.bat installer_%prg_name%.ps1 docker_command.ps1 launcher.ps1
-	SET icon=intellij-idea.ico
+	SET icon=icon.ico
 	winrar a -r -m0 -ep1 -inul -ibck -y -sfxdefault64.sfx -iadm -cfg -x*git* -iicon"..\%icon%" -z"xfs.conf" ..\output\%prg_name%.exe %files%
 :EOF
