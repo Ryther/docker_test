7d6424157d54def0213d4a802ec145c115c5032b
Edoardo Zanoni <edoardo.zanoni@gambabruno.it>
2017-11-13 08:42:21 +0100

    ~ make.bat: impostato sistema compressione 64->32bit

--- installer/windows/make.bat
+++ installer/windows/make.bat
@@ -7,5 +7,5 @@
 	SET prg_name=intellij-idea
 	SET files=lib raiser.bat installer_%prg_name%.ps1 docker_command.ps1 launcher.ps1
 	SET icon=icon.ico
-	winrar a -r -m0 -ep1 -inul -ibck -y -sfxdefault64.sfx -iadm -cfg -x*git* -iicon"..\%icon%" -z"xfs.conf" ..\output\%prg_name%.exe %files%
+	winrar a -r -m0 -ep1 -inul -ibck -y -sfxdefault.sfx -iadm -cfg -x*git* -iicon"..\%icon%" -z"xfs.conf" ..\output\%prg_name%.exe %files%
 :EOF
